f452c595a17cc833bae2bc378d0559a9ca53c9a5
Hoàng Tấn <killerdrug007@gmail.com>
2023-11-23 17:15:33 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,7 +45,92 @@ const handleViewPass = function () {
 	});
 }
 
+const handleInitSelect2 = function () {
+	if ($('.initSelect2').length) {
+		$('.initSelect2').each(function () {
+			let dropdownParent = $(this).parent();
+			$(this).select2({
+				language: 'vi',
+				dropdownParent: dropdownParent,
+				width: '100%',
+			}).one('select2:open', function(e) {
+				$('input.select2-search__field').prop('placeholder', $(this).attr('data-placeholder-search'));
+			});
+		})
+	}
+}
+
+const handleInitDateRangePicker = function () {
+	let format = 'DD-MM-YYYY';
+	let timePicker = false;
+	let timePicker24Hour = false;
+	let timePickerSeconds = false;
+	let timePickerIncrement = 5; // step minute
+	$('.initDateRangePicker').each(function () {
+		const wrapper = $(this).closest('.template-floating');
+		let type = $(this).attr('data-type');
+		if (typeof type != "undefined") {
+			if (type === 'full') {
+				format = 'DD-MM-YYYY HH:mm';
+				timePicker = true;
+				timePicker24Hour = true;
+			} else if (type === 'time') {
+				format = 'HH:mm';
+				timePicker = true;
+				timePicker24Hour = true;
+			}
+		}
+		const initDateRangePicker = $(this).daterangepicker({
+			singleDatePicker: true,
+			alwaysShowCalendars: true,
+			timePicker: timePicker,
+			timePicker24Hour: timePicker24Hour,
+			timePickerSeconds: timePickerSeconds,
+			timePickerIncrement: timePickerIncrement,
+			parentEl: wrapper,
+			autoApply: true,
+			locale: {
+				format: format,
+				daysOfWeek: [
+					"CN",
+					"T2",
+					"T3",
+					"T4",
+					"T5",
+					"T6",
+					"T7"
+				],
+				monthNames: [
+					"Tháng 1",
+					"Tháng 2",
+					"Tháng 3",
+					"Tháng 4",
+					"Tháng 5",
+					"Tháng 6",
+					"Tháng 7",
+					"Tháng 8",
+					"Tháng 9",
+					"Tháng 10",
+					"Tháng 11",
+					"Tháng 12"
+				],
+				applyLabel: 'Áp dụng',
+				cancelLabel: 'Đóng',
+			}
+		});
+
+		if (typeof type != "undefined" && type === 'time') {
+			initDateRangePicker.on('show.daterangepicker', function (ev, picker) {
+				picker.container.find(".drp-calendar").addClass('px-3');
+				picker.container.find(".calendar-table").hide();
+			});
+		}
+	})
+}
+
 $(function () {
 	handleCopyValue();
 	handleViewPass();
+	handleInitSelect2();
+	handleInitDateRangePicker();
 });
